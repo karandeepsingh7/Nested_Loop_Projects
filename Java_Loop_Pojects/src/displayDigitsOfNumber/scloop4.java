87501b1a5383b2c702a907ba4ee2ad0363260ffb
work <work@localhost>
2025-03-04 21:36:30 -0500

    Display Digits

---
+++ Java_Loop_Pojects/src/displayDigitsOfNumber/scloop4.java
@@ -0,0 +1,22 @@
+package displayDigitsOfNumber;
+import java.util.*;
+
+public class scloop4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("Enter Number for digits : ");
+			
+			int input = sc.nextInt();
+			
+			while (input > 0)
+			{
+				int r = input%10;
+				input = input/10;
+				System.out.println(r);
+			}
+	}
+
+}
