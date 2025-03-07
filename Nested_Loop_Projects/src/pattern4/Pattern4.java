92b18a8b12a0b14f54ceac06211a767e561e36e5
work <work@localhost>
2025-03-07 00:08:59 -0500

    Patterns till 4

---
+++ Nested_Loop_Projects/src/pattern4/Pattern4.java
@@ -0,0 +1,20 @@
+package pattern4;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=5; j++)
+			{
+				count++;
+				System.out.print(count+" ");
+				
+			}
+			System.out.println("");
+		}
+	}
+
+}
