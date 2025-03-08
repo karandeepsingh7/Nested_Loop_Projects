251c4d0f312df2da4df70eb4abf31dd4a458e3d7
work <work@localhost>
2025-03-07 21:36:11 -0500

    pattern 5

--- Nested_Loop_Projects/src/pattern5/Pattern5.java
+++ Nested_Loop_Projects/src/pattern5/Pattern5.java
@@ -1,16 +1,15 @@
-package pattern4;
+package pattern5;
 
-public class Pattern4 {
+public class Pattern5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int count = 0;
 		for(int i=1; i<=5; i++)
 		{
-			for(int j=1; j<=5; j++)
+			for(int j=1; j<=i; j++)
 			{
-				count++;
-				System.out.format("%02d",count);
+			
+				System.out.print(j+ "");
 				
 			}
 			System.out.println("");
